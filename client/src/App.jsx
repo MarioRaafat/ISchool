a6dcd33ae2b8bacea08b7629f29fbd47edd09b0f
Mario Raafat <mariorafat10@gmail.com>
2024-10-19 09:42:03 +0300

    fixing some mistakes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
 import Auth from "@/components/Auth/Auth.jsx";
 
 function App() {
@@ -7,11 +7,14 @@ function App() {
   return (
     <BrowserRouter>
         <Routes>
-            <Route path="/author" element={
+            <Route path="/auth" element={
               <Auth />
             } />
 
-            <Route path="*" element={<Auth/>} />
+            <Route path="*" element={
+                <Navigate to="/auth"/>
+                }
+            />
         </Routes>
     </BrowserRouter>
   )
