9b2a6b635c7332ad9d397159c35ef2790584311f
mahmoud malek <information.mahmoud@gmail.com>
2024-10-31 20:12:37 +0300

    add teacher_subjects model

---
+++ server/models/teacher_subjects.js
@@ -0,0 +1,31 @@
+// server/models/teacher_subjects.js
+'use strict';
+export default (sequelize, DataTypes) => {
+	const TeacherSubjects = sequelize.define('TeacherSubjects', {
+		teacher_id: {
+			type: DataTypes.UUID,
+			allowNull: false,
+			references: {
+				model: 'Teachers',
+				key: 'id'
+			},
+			onDelete: 'CASCADE'
+		},
+		subject_id: {
+			type: DataTypes.UUID,
+			allowNull: false,
+			references: {
+				model: 'Subjects',
+				key: 'id'
+			},
+			onDelete: 'CASCADE'
+		}
+	}, {
+		timestamps: true
+	});
+	TeacherSubjects.associate = function (models) {
+		TeacherSubjects.belongsTo(models.Teacher, { foreignKey: 'teacher_id', as: 'Teacher' });
+		TeacherSubjects.belongsTo(models.Subject, { foreignKey: 'subject_id', as: 'Subject' });
+	};
+	return TeacherSubjects;
+};
